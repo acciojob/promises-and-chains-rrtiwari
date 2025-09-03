0e71f74e13be92ba5365fa4655b3770f882ce397
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-09-03 11:37:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+let form = document.getElementById("myform");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let age = parseInt(document.getElementById("age").value);
+  let name = document.getElementById("name").value.trim();
+
+  validate(age, name)
+    .then((msg) => alert(msg))
+    .catch((err) => alert(err));
+});
+
+function validate(age, name) {
+  if (!age || !name) {
+    return Promise.reject("Please enter valid details.");
+  }
+
+  if (age < 18) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject("Oh sorry. You aren't old enough.");
+      }, 4000);
+    });
+  } else {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(`Welcome, ${name}. You can vote.`);
+      }, 4000);
+    });
+  }
+}
+
+
